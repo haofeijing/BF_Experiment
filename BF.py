795fee80aa0ad1fd4ecd77c3453b609188d94926
hanyinbin <hyb117010083@outlook.com>
2020-06-26 23:42:52 +0800

    Jun 26, 11:42. I am lazy and tired.

--- BF.py
+++ BF.py
@@ -6,15 +6,30 @@ np.random.seed(234198)
 from matplotlib import pyplot as plt
 
 
+class stock:
+    def __init__(self, Ks, Kb, T, r):
+        self.Ks = Ks
+        self.Kb = Kb
+        self.T = T
+        self.r = r
+
+
+
+    def g(self, n, m, X):
+        return np.log(self.X[n, m, :]*(1 - self.Ks) + self.r*(self.T - n))
+
+
 # Simulating geometric Brownian motion
-def stock_sim_path(S, alpha, delta, sigma, T, N, n):
+def stock_sim_path(S, alpha, delta, sigma, T, N, n, M):
     """Simulates geometric Brownian motion."""
     h = T/n
     # uncomment below for deterministic trend. or, can pass it in as alpha as an array
     alpha = alpha # + np.linspace(0, 0.1, 500).reshape((n,N))
     mean = (alpha - delta - .5*sigma**2)*h
     vol = sigma * h**.5
-    return S*np.exp((mean + vol*np.random.randn(n,N)).cumsum(axis = 0))
+    return S*np.exp((mean + vol*np.random.randn(n,N, M)).cumsum(axis = 0))
+
+
 
 
 class NeuralNet(torch.nn.Module):
@@ -50,20 +65,47 @@ def NN(n, x, s, tau_n_plus_1):
     model = NeuralNet(s.d, s.d + 40, s.d + 40)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 
+    F = None
     for epoch in range(epochs):
-        F = model.forward(X[n])
+        F = model.forward(x[n])
         optimizer.zero_grad()
-        criterion = loss(F, S, X, n, tau_n_plus_1)
+        criterion = loss(F, s, x, n, tau_n_plus_1)
         criterion.backward()
         optimizer.step()
 
     return F, model
 
 
-if __name__ == "__main__":
+def training():
     T = 2
     days = int(250*T)
-    stock_path = stock_sim_path(100, 0.05, 0, .15, T, 1, days)
-    print(stock_path)
-    # plt.plot(np.arange(days), stock_path)
+    M = 50
+    stock_path = stock_sim_path(100, 0.05, 0, .15, T, 1, days, M)
+
+    X = torch.from_numpy(stock_path).float()
+    print(X)
+    # for m in range(M):
+    #     plt.plot(np.arange(days), stock_path[:, :, m])
     # plt.show()
+
+    mods = [None] * days  # models (para) at each time step
+    tau_mat = np.zeros((days + 1, M))
+    tau_mat[days, :] = days
+
+    f_mat = np.zeros((days + 1, M))
+    f_mat[days, :] = 1
+
+    # %%
+    for n in range(days - 1, -1, -1):
+        probs, mod_temp = NN(n, X, S, torch.from_numpy(tau_mat[n + 1]).float())
+        mods[n] = mod_temp
+        np_probs = probs.detach().numpy().reshape(M)  # probs for each asset in each path at time n
+        print(n, ":", np.min(np_probs), " , ", np.max(np_probs))
+
+        f_mat[n, :] = (np_probs > 0.5) * 1.0
+
+        tau_mat[n, :] = np.argmax(f_mat, axis=0)
+
+
+if __name__ == "__main__":
+    training()
